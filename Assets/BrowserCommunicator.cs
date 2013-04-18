4d5bac79959ae2bb42bfe05bc0489dfedd6814af
Eric Mellino <raverenx@gmail.com>
2013-04-18 18:25:42 -0400

    Added ConnectToGame function

--- Assets/BrowserCommunicator.cs
+++ Assets/BrowserCommunicator.cs
@@ -8,4 +8,15 @@ public class BrowserCommunicator : MonoBehaviour {
         Application.ExternalEval("alert('Finished Loading!');");
         yield break;
     }
+
+    void ConnectToGame(string message)
+    {
+        var split = message.Split('\n');
+        var ipAddress = split[0];
+        var port = int.Parse(split[1]);
+        var teamNumber = int.Parse(split[2]);
+
+        Network.Connect(ipAddress, port);
+
+    }
 }
